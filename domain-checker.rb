89cfbdb3973c5b789c4e71bb7de9dfba89ecb964
Justin Hearn <justin@hearn.me>
2016-10-16 15:59:50 -0700

    increase whois timeout

--- domain-checker.rb
+++ domain-checker.rb
@@ -5,8 +5,10 @@ input_list = ARGV.first
 input_file = open(input_list)
 
 input_file.each_line { |domain|
-	record = Whois.whois(domain.downcase.chomp)
-	puts record.registrar.name
+	whois = Whois::Client.new(:timeout => 10)
+	record = whois.lookup(domain.downcase.chomp)
+	puts domain
+	puts record.registrar
 }
 
 input_file.close
