b40bc26d945f9cc97f190b5df1fbf52864444ac4
Justin Hearn <justin@hearn.me>
2016-10-16 16:30:45 -0700

    verifies GoDaddy registrar

--- domain-checker.rb
+++ domain-checker.rb
@@ -7,8 +7,27 @@ input_file = open(input_list)
 input_file.each_line { |domain|
 	whois = Whois::Client.new(:timeout => 10)
 	record = whois.lookup(domain.downcase.chomp)
-	puts domain
-	puts record.registrar
+	registrar_url = record.registrar.url
+	registrar_name = record.registrar.name
+	
+	gd_url = false
+	gd_name = false
+
+	if registrar_url == nil then
+#		puts "#{domain.chomp} registrar has no URL."
+	elsif registrar_url.downcase =~ /.*godaddy.*/
+		gd_url = true
+	end
+
+	if registrar_name == nil then
+#		puts "#{domain.chomp} registrar has no name."
+	elsif registrar_name.downcase =~ /.*godaddy.*/
+		gd_name = true
+	end
+	
+	if (gd_url || gd_name) then
+		puts "#{domain.chomp} registrar is GoDaddy."
+	end
 }
 
 input_file.close
